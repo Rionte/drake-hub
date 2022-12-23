aea6306b3dfbeb2a348d13d13455004746721923
Rionte <ramonsabro@gmail.com>
2022-12-23 16:55:36 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,14 @@
+--local ss = loadstring(game:HttpGet("https://raw.githubusercontent.com/exxtremestuffs/SimpleSpySource/master/SimpleSpy.lua"))()
+
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
 local Window = Library.CreateLib("Drake Hub", "Sentinel")
 local UserInputService = game:GetService("UserInputService")
-local lplayer = game.Workspace:WaitForChild(game.Players.LocalPlayer.Name)
+local lplayer = game.Workspace:FindFirstChild(game.Players.LocalPlayer.Name)
 local pname = game.Players.LocalPlayer.Name
 local kaDistance = 10;
 local kaSpeed = 1;
+local kaState = false;
+local bhopState = false;
 
 getgenv().Toggled = false
 
@@ -24,6 +28,7 @@ local welcome_message = general:NewSection("Welcome, " .. game.Players.LocalPlay
 local sword_anims = pilpir:NewSection("Sword Animations")
 
 local killaurasection = minershaft:NewSection("Killaura")
+local movement = minershaft:NewSection("Movement")
 local world = minershaft:NewSection("World")
 
 -- General
@@ -51,12 +56,25 @@ end)
 
 -- MinerShaft
 
-local killaura = killaurasection:NewToggle("Activate", "Activate Killaura", function(state)
+killaurasection:NewKeybind("Killaura", "Activate Killaura", Enum.KeyCode.G, function()
+	if kaState then
+        kaState = false
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Killaura",
+            Text = "Killaura Disabled",
+        })
+    else
+        kaState = true
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Killaura",
+            Text = "Killaura Enabled",
+        })
+    end
 
-    getgenv().Toggled = state
-
-    while getgenv().Toggled do
-        getgenv().Toggled = state
+    while kaState do
+        if kaState == false then
+            break
+        end
 
         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
             if player:DistanceFromCharacter(lplayer.HumanoidRootPart.Position) > kaDistance or player.Name == pname then
@@ -70,7 +88,7 @@ local killaura = killaurasection:NewToggle("Activate", "Activate Killaura", func
             end
         end
         
-        wait(kaSpeed)
+        wait(0.3)
     end
 end)
 
@@ -78,20 +96,11 @@ local kaDistanceSlider = killaurasection:NewSlider("Range", "Set the Range for K
     kaDistance = s;
 end)
 
-local kaDistanceSlider = killaurasection:NewSlider("Range", "Set the Range for Killaura", 3, 0.1, function(s)
+local kaDistanceSlider = killaurasection:NewSlider("Speed", "Set the Speed for Killaura", 3, 0.1, function(s)
     kaSpeed = s;
 end)
 
-local suicide = world:NewButton("Suicide", "Kills the player!", function()
-    local args = {
-        [1] = 100,
-        [2] = "fall"
-    }
-    
-    game:GetService("ReplicatedStorage").GameRemotes.Demo:FireServer(unpack(args))
-end)
-
-local infjump = world:NewToggle("Infinite Jump", "Toggle Infinite Jump", function(state)
+--[[ local infjump = movement:NewToggle("Infinite Jump", "Toggle Infinite Jump", function(state)
 
     getgenv().Toggled = state
 
@@ -108,4 +117,64 @@ local infjump = world:NewToggle("Infinite Jump", "Toggle Infinite Jump", functio
 
         wait(1)
     end
+end) ]]
+
+movement:NewKeybind("Bhop", "Bhop", Enum.KeyCode.B, function()
+	if bhopState then
+        bhopState = false
+        lplayer.Humanoid.WalkSpeed = 16
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Bhop",
+            Text = "Bhop Disabled",
+        })
+    else
+        bhopState = true
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Bhop",
+            Text = "Bhop Enabled",
+        })
+    end
+
+    while bhopState do
+        if bhopState == false then
+            break
+        end
+
+        lplayer.Humanoid.Jump = true
+        lplayer.Humanoid.WalkSpeed = 30
+        
+        wait()
+    end
+end)
+
+world:NewButton("Ore Scanner", "Find all ores in a radius", function()
+
+    local ores = Instance.new("Model")
+    ores.Name = "Ores"
+    ores.Parent = game.Workspace:FindFirstChild("Blocks")
+
+    for _, chunk in pairs(game.Workspace:FindFirstChild("Blocks"):GetChildren()) do
+        for _, block in pairs(chunk:GetChildren()) do
+            if block.Name:sub(-string.len("Ore")) == "Ore" then
+                block.Parent = game.Workspace:FindFirstChild("Blocks"):FindFirstChild("Ores")
+            end
+        end
+    end
+
+    highlight = Instance.new("Highlight")
+    highlight.Parent = game.Workspace:FindFirstChild("Blocks"):FindFirstChild("Ores")
+
+end)
+
+world:NewButton("Godmode", "Become Jesus Himself", function()
+    
+end)
+
+local suicide = world:NewButton("Suicide", "Kills the player!", function()
+    local args = {
+        [1] = 1,
+        [2] = "fall"
+    }
+    
+    game:GetService("ReplicatedStorage").GameRemotes.Demo:FireServer(unpack(args))
 end)
