d2e2a65023cd3ff4f86e1324a0d583e99f1f5838
Rionte <ramonsabro@gmail.com>
2022-12-28 15:52:35 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -26,7 +26,10 @@ local closest = "";
 local invState = false;
 local breakState = false;
 local inv = {};
-local invFilters = {};
+local invFilters = {"diamond", "ruby", "sapphire", "coal", "bread", "banana", "apple"};
+local prevOn = false;
+local guiInstance;
+local isGuiDestroyed = false;
 
 game:GetService("StarterGui"):SetCore("SendNotification",{
 	Title = "Loaded!",
@@ -52,20 +55,26 @@ end
 
 -- Tabs
 local general = Window:NewTab("General")
-local minershaft = Window:NewTab("MinerShaft")
-local debug = Window:NewTab("Debug")
+local combatTab = Window:NewTab("Combat")
+local movementTab = Window:NewTab("Movement")
+local renderTab = Window:NewTab("Render")
+local worldTab = Window:NewTab("World")
 
 -- Sections
 local welcome_message = general:NewSection("Welcome, " .. game.Players.LocalPlayer.Name .. "!")
-
-local killaurasection = minershaft:NewSection("Killaura")
-local world = minershaft:NewSection("World")
-local movement = minershaft:NewSection("Movement")
-local render = minershaft:NewSection("Render")
+local killauraSection = combatTab:NewSection("Killaura")
+local bhopSection = movementTab:NewSection("Bhop")
+local infjumpSection = movementTab:NewSection("Infinite Jump")
+local invcleanSection = worldTab:NewSection("Inventory Cleaner")
+local playertpSection = worldTab:NewSection("Player TP")
+local godSection = worldTab:NewSection("God Mode")
+local xraySection = renderTab:NewSection("Xray")
+local fullbrightSection = renderTab:NewSection("Fullbright")
+local espSection = renderTab:NewSection("ESP")
 
 -- General
 welcome_message:NewKeybind("Toggle UI", "Toggle the UI", Enum.KeyCode.End, function()
-	Library:ToggleUI()
+    Library:ToggleUI()
 end)
 
 local nmode = welcome_message:NewToggle("Spam", "Spam the Chat", function(state)
@@ -85,16 +94,27 @@ local nmode = welcome_message:NewToggle("Spam", "Spam the Chat", function(state)
 end)
 
 -- MinerShaft
-killaurasection:NewKeybind("Killaura", "Activate Killaura", Enum.KeyCode.G, function()
-	if kaState then
+killauraSection:NewKeybind("Killaura", "Activate Killaura", Enum.KeyCode.G, function()
+    if kaState then
         kaState = false
-        closest = ""
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = "Killaura",
             Text = "Killaura Disabled",
         })
     else
         kaState = true
+        closest = "";
+        for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+            if player.Name == game.Players.LocalPlayer.Name == false then
+                if closest == "" then
+                    closest = player
+                else
+                    if player:DistanceFromCharacter(lplayer.HumanoidRootPart.Position) < game.Players.LocalPlayer:DistanceFromCharacter(closest.Character.HumanoidRootPart.Position) then
+                        closest = player
+                    end
+                end
+            end
+        end
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = "Killaura",
             Text = "Killaura Enabled",
@@ -104,7 +124,7 @@ killaurasection:NewKeybind("Killaura", "Activate Killaura", Enum.KeyCode.G, func
     while kaState do
         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
             if player:DistanceFromCharacter(lplayer.HumanoidRootPart.Position) > kaDistance or player.Name == pname then
-                continue
+                do end
             else
                 if closest == "" then
                     closest = player
@@ -113,39 +133,43 @@ killaurasection:NewKeybind("Killaura", "Activate Killaura", Enum.KeyCode.G, func
                         closest = player
                     end
                 end
-
-                if closest:DistanceFromCharacter(lplayer.HumanoidRootPart.Position) < kaDistance then
-                    local args = {
-                        [1] = game:GetService("Players"):FindFirstChild(closest.Name).Character
-                    }
-
-                    game:GetService("ReplicatedStorage").GameRemotes.Attack:InvokeServer(unpack(args))
-                end
             end
+        end
+
+        if closest:DistanceFromCharacter(lplayer.HumanoidRootPart.Position) < kaDistance then
+            local args = {
+                [1] = game:GetService("Players"):FindFirstChild(closest.Name).Character
+            }
+
+            game:GetService("ReplicatedStorage").GameRemotes.Attack:InvokeServer(unpack(args))
         end
         
         wait(0.6)
     end
 end)
 
-local kaDistanceSlider = killaurasection:NewSlider("Range", "Set the Range for Killaura", 20, 1, function(s)
+local kaDistanceSlider = killauraSection:NewSlider("Range", "Set the Range for Killaura", 20, 1, function(s)
     kaDistance = s;
 end)
 
-local kaSpeedSlider = killaurasection:NewSlider("Speed", "Set the Speed for Killaura", 1, 0, function(s)
+local kaSpeedSlider = killauraSection:NewSlider("Speed", "Set the Speed for Killaura", 1, 0, function(s)
     kaSpeed = s;
 end)
 
-movement:NewKeybind("Bhop", "Bhop", Enum.KeyCode.B, function()
-	if bhopState then
+bhopSection:NewKeybind("Bhop", "Bhop", Enum.KeyCode.B, function()
+    if bhopState then
         bhopState = false
         lplayer.Humanoid.WalkSpeed = 16
+        if prevOn then
+            infjumpState = true
+        end
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = "Bhop",
             Text = "Bhop Disabled",
         })
     else
         bhopState = true
+        infjumpState = false
         game:GetService("StarterGui"):SetCore("SendNotification",{
             Title = "Bhop",
             Text = "Bhop Enabled",
@@ -153,10 +177,6 @@ movement:NewKeybind("Bhop", "Bhop", Enum.KeyCode.B, function()
     end
 
     while bhopState do
-        if bhopState == false then
-            break
-        end
-
         lplayer.Humanoid.Jump = true
         lplayer.Humanoid.WalkSpeed = bhopSpeed
         
@@ -164,28 +184,30 @@ movement:NewKeybind("Bhop", "Bhop", Enum.KeyCode.B, function()
     end
 end)
 
-movement:NewSlider("Bhop Speed", "Set speed for Bhop", 100, 10, function(s)
+bhopSection:NewSlider("Bhop Speed", "Set speed for Bhop", 100, 10, function(s)
     bhopSpeed = s;
 end)
 
-local infjump = movement:NewToggle("Infinite Jump", "Toggle Infinite Jump", function(state)
+local infjump = infjumpSection:NewToggle("Infinite Jump", "Toggle Infinite Jump", function(state)
 
     infjumpState = state
 
     if infjumpState then
+        prevOn = true;
         game:GetService("UserInputService").JumpRequest:connect(function()
             if infjumpState then
+                bhopState = false
                 game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Jumping")
             else
                 game:GetService("Players").LocalPlayer.Character:FindFirstChildOfClass('Humanoid'):ChangeState("Standing")
             end
         end)
-
-        wait(1)
+    else
+        prevOn = false;
     end
 end)
 
-world:NewKeybind("Inv Cleaner", "Clean your Inventory", Enum.KeyCode.L, function()
+invcleanSection:NewKeybind("Inv Cleaner", "Clean your Inventory", Enum.KeyCode.L, function()
     if invState then
         invState = false
         game:GetService("StarterGui"):SetCore("SendNotification",{
@@ -204,9 +226,7 @@ world:NewKeybind("Inv Cleaner", "Clean your Inventory", Enum.KeyCode.L, function
         inv = {}
         for index, item in pairs(lplayer:FindFirstChild("Inventory"):GetChildren()) do
             local itemTable = game.HttpService:JSONDecode(lplayer:FindFirstChild("Inventory"):FindFirstChild(item.Name).Value)
-            if itemTable.count == 0 then
-                continue
-            else
+            if itemTable.count > 0 then
                 inv[itemTable.name .. index-1] = index-1
             end
         end
@@ -234,53 +254,39 @@ world:NewKeybind("Inv Cleaner", "Clean your Inventory", Enum.KeyCode.L, function
     end
 end)
 
-world:NewTextBox("Inv Cleaner Filters", "Filter Words Using Spaces", function(txt)
-    invFilters = {}
+invcleanSection:NewTextBox("Inv Cleaner Filters", "Filter Words Using Spaces", function(txt)
     for v in string.gmatch(txt, "%S+") do
         table.insert(invFilters, string.lower(v))
     end
 end)
 
-world:NewKeybind("Instabreak (ONLY DIRT)", "Insta-mine any Block", Enum.KeyCode.P, function()
-    if breakState then
-        breakState = false
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Insta Break",
-            Text = "Insta Break Disabled",
-        })
-    else
-        breakState = true
-        game:GetService("StarterGui"):SetCore("SendNotification",{
-            Title = "Insta Break",
-            Text = "Insta Break Enabled",
-        })
-    end
-
-    while breakState do
-        SimpleSpy:GetRemoteFiredSignal(game:GetService("ReplicatedStorage").GameRemotes.BreakBlock):Connect(function(args)
-            if breakState then
-                wait()
-                game:GetService("ReplicatedStorage").GameRemotes.AcceptBreakBlock:InvokeServer()
-            end
-        end)
-        
-        wait()
+invcleanSection:NewTextBox("Remove Filter", "Remove a Filter", function(txt)
+    for k, v in pairs(invFilters) do
+        if string.lower(txt) == v then
+            table.remove(invFilters, k)
+        end
     end
 end)
 
-world:NewTextBox("Player Name", "Input the Players Name", function(txt)
+invcleanSection:NewButton("Clear Filters", "Clear all Filters", function()
+    for k, _ in pairs(invFilters) do
+        table.remove(invFilters, k)
+    end
+end)
+
+playertpSection:NewTextBox("Player Name", "Input the Players Name", function(txt)
     tpName = txt;
 end)
 
-world:NewButton("Teleport to Player", "Click to Teleport", function()
+playertpSection:NewButton("Teleport to Player", "Click to Teleport", function()
     lplayer.HumanoidRootPart.CFrame = game.Workspace:FindFirstChild(tpName).HumanoidRootPart.CFrame
 end)
 
-world:NewButton("Godmode (Except Player Damage)", "Become Jesus Himself", function()
+godSection:NewButton("Godmode (Except Player Damage)", "Become Jesus Himself", function()
     SimpleSpy:BlockRemote("Demo")
 end)
 
-render:NewKeybind("Xray", "Find all ores in a radius", Enum.KeyCode.X, function()
+xraySection:NewKeybind("Xray", "Find all ores in a radius", Enum.KeyCode.X, function()
 
     if xrayState then
         xrayState = false
@@ -314,7 +320,7 @@ render:NewKeybind("Xray", "Find all ores in a radius", Enum.KeyCode.X, function(
     end
 end)
 
-render:NewToggle("Fullbright", "Brighten up the world!", function(state)
+fullbrightSection:NewToggle("Fullbright", "Brighten up the world!", function(state)
     fullbrightState = state
 
     if fullbrightState then
@@ -326,28 +332,45 @@ render:NewToggle("Fullbright", "Brighten up the world!", function(state)
     end
 end)
 
-render:NewSlider("Fullbright Brightness", "Adjust the Brightness for Fullbright", 100, 1, function(s)
+fullbrightSection:NewSlider("Fullbright Brightness", "Adjust the Brightness for Fullbright", 100, 1, function(s)
     brightness = s;
 end)
 
-render:NewToggle("Player ESP", "Light Up Players", function(state)
+espSection:NewToggle("Player ESP", "Light Up Players", function(state)
     espState = state
 
-    if espState then
-        for _, player in pairs(game:GetService("Players"):GetPlayers()) do
-            esp = Instance.new("Highlight")
-            esp.Name = "esp"
-            esp.FillColor = Color3.new(0, 0, 255)
-            esp.Parent = player.Character
-        end
-    else
+    if not espState then
+        espState = false
         for _, player in pairs(game:GetService("Players"):GetPlayers()) do
             player.Character:FindFirstChild("esp"):Destroy()
         end
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Player ESP",
+            Text = "Player ESP Disabled",
+        })
+    else
+        espState = true
+        game:GetService("StarterGui"):SetCore("SendNotification",{
+            Title = "Player ESP",
+            Text = "Player ESP Enabled",
+        })
+    end
+
+    while espState do
+        for _, player in pairs(game:GetService("Players"):GetPlayers()) do
+            if not player.Character:FindFirstChildOfClass("Highlight") then
+                esp = Instance.new("Highlight")
+                esp.Name = "esp"
+                esp.FillColor = Color3.new(0, 0, 255)
+                esp.Parent = player.Character
+            end
+        end
+        
+        wait(5)
     end
 end)
 
-render:NewToggle("Storage ESP", "Light Up Chests", function(state)
+espSection:NewToggle("Storage ESP", "Light Up Chests", function(state)
     sespState = state
 
     if sespState then
